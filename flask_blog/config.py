8478d671eabdfec809ae81af266eff38f3f8801e
yuji <w074ff@yamaguchi-u.ac.jp>
2020-10-12 20:13:06 +0900

    最初のコミット　テンプレート作成中

---
+++ flask_blog/config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+USERNAME = 'yuji'
+PASSWORD = '0705'
---
+++ flask_blog/views.py
@@ -0,0 +1,31 @@
+from flask import request, redirect, url_for, render_template, flash, session
+from flask_blog import app
+
+@app.route("/")
+def show_entries():
+    s = '普通'
+    return render_template('entries/index.html', status = s)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            print('ユーザー名が異なります')
+        elif request.form['password'] != app.config['PASSWORD'] :
+            print('パスワードが異なります。')
+        else:
+            return redirect('/')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
+
+
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
